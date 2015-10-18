9dd35566382de843e65e3f84651d5d4ecaaa93e2
cong <783010296@qq.com>
2015-10-18 23:52:59 +0800

    搜索框下面的2个东东的切换效果

---
+++ WebClient/js/main.js
@@ -0,0 +1,40 @@
+var number = 1;
+var test = "id";
+function chatwith(e){
+  $(".main-right").show();
+  var name = $(e).children(".chatmessage-1").children(".chatmessage-1-info").children("h4").text();
+  $(".main-right .main-right-nav h2").text(name);
+  };
+function sendto(){
+  var testid = test + number ;
+  /**
+  var $html = $("<div class='main-right-chatmessage-1'>"
+  +"<div class='main-right-chatmessage-1-my' id=" + testid + "></div>"
+  +"<div class='chatmessage-1-image' style='float:right;'>"
+  +"<img src='cong.jpg' width='40px' height='40px'/>"
+  +"</div>"
+  +"</div>");
+  **/
+  var $html =$("<div class='content'>"     
+		+"<div class='chatmessage-1-image' style='float:right;'>"
+		+"<img src='image/cong.jpg' width='40px' height='40px'/>"
+		+"</div>"
+		+"<div class='bubble'>"
+		+"<div class='bubble_cont'>"
+		+"<div class='plain'>"
+		+"<pre class='js_message_plain ng-binding' id=" + testid +"></pre>"
+		+"</div>"
+		+"</div>"
+		+"</div>"
+		+"</div>");
+  $(".main-right-chatmessage").append($html);
+  var message = $(".main-right-writemessage").val();
+  var idtest = "#" + testid ;
+  $(idtest).text(message);
+  $(".main-right-writemessage").val("");
+  number = number + 1;
+};
+function change_web_wechat_tab_chat(e){
+	$(e).children("i").css("background","url(./image/sprite27fe59.png) 0 -2083px");
+	};
+  
