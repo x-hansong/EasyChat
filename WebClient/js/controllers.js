7f1730754ef91d8898c28687d3535a2d34a087ae
zl36192031 <619177886@qq.com>
2015-11-28 20:59:51 +0800

    加了注册接口

--- WebClient/js/controllers.js
+++ WebClient/js/controllers.js
@@ -1,1 +1,19 @@
-var weChatCtrls=angular.module('weChatCtrls', []);
+//注册控制模块
+var registerCtrls=angular.module('registerCtrls', []);
+registerCtrls.controller('registerCtrl1',function($scope,$http){
+	$scope.registerMessage={
+		name:"",
+		password:""
+	};
+	$scope.registerSub=function(){
+		 $http.post('http://119.29.26.47/v1/users',$scope.registerMessage)
+	        .success(function(data) {
+	            console.log(data);
+				})
+	        .error(function(data){
+	        	alert("an unexpected error ocurred!");
+	        	 console.log(data);
+    });
+
+	};
+})
