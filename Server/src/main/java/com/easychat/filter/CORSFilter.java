c7d92a1c2d01654542c91c2a8fe79c950d3f8c01
x-hansong <x.hansong.scut@gmail.com>
2015-12-17 15:59:00 +0800

    修改filter

---
+++ Server/src/main/java/com/easychat/filter/CORSFilter.java
@@ -0,0 +1,35 @@
+package com.easychat.filter;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by xhans on 2015/12/13.
+ */
+/*
+    跨域请求过滤器，添加CORS相关请求头
+ */
+@Component
+public class CORSFilter extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        response.setHeader("Access-Control-Allow-Origin", "*");
+        if (request.getHeader("Access-Control-Request-Method") != null && "OPTIONS".equals(request.getMethod())) {
+            // CORS "pre-flight" request
+            response.addHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+            response.addHeader("Access-Control-Allow-Headers", "origin, content-type, accept,x-auth-token");
+            response.setHeader("Access-Control-Expose-Headers","x-auth-token");
+            response.addHeader("Access-Control-Max-Age", "1");
+        }
+
+        filterChain.doFilter(request, response);
+    }
+
+
+}
