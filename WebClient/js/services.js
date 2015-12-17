1203c82e3f8c149c7dcee754ac26c3a9c3848a37
zl36192031 <619177886@qq.com>
2015-12-17 21:29:10 +0800

    修改一些接口处理

--- WebClient/js/services.js
+++ WebClient/js/services.js
@@ -1,9 +1,7 @@
 var weChatServices = angular.module('weChatServices', []);
 
-weChatServices.service('weChatServices_1', ['$scope',
-    function($scope) {}
-]);
-
-weChatServices.service('weChatServices_2', ['$scope',
-    function($scope) {}
+weChatServices.service('chatSocket', ['$scope','$timeout'
+    function($scope,$timeout) {
+    	sdfd
+    }
 ]);
