66e6db54509160c02d2a50cb9086f3e2e1fe0c99
x-hansong <x.hansong.scut@gmail.com>
2015-12-17 16:04:27 +0800

    修改filter

--- Server/src/main/java/com/easychat/filter/CORSFilter.java
+++ Server/src/main/java/com/easychat/filter/CORSFilter.java
@@ -20,11 +20,11 @@ public class CORSFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         response.setHeader("Access-Control-Allow-Origin", "*");
+        response.setHeader("Access-Control-Expose-Headers","x-auth-token");
         if (request.getHeader("Access-Control-Request-Method") != null && "OPTIONS".equals(request.getMethod())) {
             // CORS "pre-flight" request
             response.addHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
             response.addHeader("Access-Control-Allow-Headers", "origin, content-type, accept,x-auth-token");
-            response.setHeader("Access-Control-Expose-Headers","x-auth-token");
             response.addHeader("Access-Control-Max-Age", "1");
         }
 
