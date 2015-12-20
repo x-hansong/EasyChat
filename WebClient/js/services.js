cbc94bb03a786714137325fc452408c8aa27561f
zl36192031 <619177886@qq.com>
2015-12-20 21:34:12 +0800

    修改用户信息接口

--- WebClient/js/services.js
+++ WebClient/js/services.js
@@ -1,95 +1,54 @@
 var weChatServices = angular.module('weChatServices', []);  //创建服务
-var ws ;
-
-if (!window.WebSocket && window.MozWebSocket)
-    window.WebSocket=window.MozWebSocket;
-if (!window.WebSocket)
-    alert("No Support ");
-weChatServices.service('weChatServices_1', ['$scope',  //服务
+weChatServices.service('chatSocket', ['$scope',  
     function($scope) {
-        function newWebSocket() {                           //socket函数
-            var wsURL = "ws://119.29.26.47:8000/ws";
-            var wst = new WebSocket(wsURL);
-            wst.onopen = function (evnt) {
-                onOpen(evnt)
-            };
-            wst.onmessage = function (evnt) {
-                onMessage(evnt)
-            };
-            wst.onclose = function (evnt) {
-                onclose(evnt)
-            };
-            wst.onerror = function (evnt) {
-                onError(evnt)
-            };
-            return wst;
+    	var stompClient=null;
+    	function connect() {
+            var socket = new SockJS('http://119.29.26.47:8080/chat');
+            stompClient = Stomp.over(socket);
+            stompClient.connect({}, function(frame) {
+                console.log('Connected: ' + frame);
+                var user = $scope.userMwssage.user.name;
+                stompClient.subscribe("/queue/channel/"+user, function(msg) {
+                    console.log("RECEIVE:"+msg);
+					var label =$("<div class='content'>"
+					    +"<div class='chatmessage-1-image' style='float:right;'>"
+					    +"<img src='image/cong.jpg' width='40px' height='40px'/>"
+					    +"</div>"
+					    +"<div class='bubble'>"
+					    +"<div class='bubble_cont'>"
+					    +"<div class='plain'>"
+					    +"<pre class='js_message_plain ng-binding'>"+msg+"</pre>"
+					    +"</div>"
+					    +"</div>"
+					    +"</div>"
+					    +"</div>");
+					$(".main-right-chatmessage").append(label);
+                });
+            });
         }
-     ws = newWebSocket();
+        //发送消息函数
+		function sendMessage()
+		{
+		    if($(".main-right-writemessage").val()==""){
+		        alert("内容不能为空！");
+		    }
+		    else {
+		        var msg = {
+		            "fromUser": $(".my-name").text(),
+		            "message": $(".main-right-writemessage").val(),
+		            "toUser": $(".you-name").text()
+		        };
+		        var msg_str = JSON.stringify(msg);
+		        send(msg_str);
+		        $(".main-right-writemessage").val("");
+		    }
+		}
 
-        function onOpen(){
-            console.log("success open");
-        }
-        function onclose(event)
-        {
-            console.log("Client notified socket has closed",event);
-        }
-        function onMessage(){
-            console.log("RECEIVE:"+event.data);
-            var $html =$("<div class='content'>"
-                +"<div class='chatmessage-1-image' style='float:right;'>"
-                +"<img src='image/cong.jpg' width='40px' height='40px'/>"
-                +"</div>"
-                +"<div class='bubble'>"
-                +"<div class='bubble_cont'>"
-                +"<div class='plain'>"
-                +"<pre class='js_message_plain ng-binding'>"+event.data+"</pre>"
-                +"</div>"
-                +"</div>"
-                +"</div>"
-                +"</div>");
-            $(".main-right-chatmessage").append($html);
-        }
-        function onError(event){
-            console.log('ERROR: ', evnt);
-        }
-
-    }
+		//子发送消息函数
+		function send(data)
+		{
+		    console.log("Send:"+data);
+		    stompClient.send("/app/chat",{},data);
+		}	
+	}
 ]);
-
-weChatServices.service('weChatServices_2', ['$scope',
-    function($scope) {}
-
-]);
-
-
-//发生消息函数
-function sendMessage()
-{
-    if($(".main-right-writemessage").val()==""){
-        alert("内容不能为空");
-    }
-    else {
-        var msg = {
-            "from": $(".my-name").text(),
-            "message": $(".main-right-writemessage").val(),
-            "to": $(".you-name").text()
-        };
-        var msg_str = JSON.stringify(msg);
-        send(msg_str);
-        $(".main-right-writemessage").val("");
-    }
-}
-
-
-function send(data)
-{
-    console.log("Send:"+data);
-   // ws.send(data);
-}
-
-function keysend(event){
-    if (event.ctrlKey && event.keyCode == 13) {
-        sendto();
-        $('.main-right-chatmessage')[0].scrollTop = $('.main-right-chatmessage')[0].scrollHeight;
-    }
-}
