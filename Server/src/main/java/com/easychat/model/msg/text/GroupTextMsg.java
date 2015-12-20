99a164e58c9c44e33b42f5f79bddc775451e9bca
x-hansong <x.hansong.scut@gmail.com>
2015-12-20 22:18:05 +0800

    增加添加好友的接口

--- Server/src/main/java/com/easychat/model/msg/text/GroupTextMsg.java
+++
@@ -1,7 +0,0 @@
-package com.easychat.model.msg.text;
-
-/**
- * Created by yonah on 15-11-9.
- */
-public class GroupTextMsg {
-}
--- Server/src/main/java/com/easychat/model/msg/text/OfflineTextMsg.java
+++
@@ -1,7 +0,0 @@
-package com.easychat.model.msg.text;
-
-/**
- * Created by yonah on 15-11-10.
- */
-public class OfflineTextMsg {
-}
